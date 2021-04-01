ec81fa8c157083292b04c478ba6ab2dece8fc472
rohanprad <rohanprad09@gmail.com>
2021-04-01 19:49:30 +0530

    Edited the Visualization Script

--- DataVisualization.R
+++ DataVisualization.R
@@ -7,20 +7,20 @@ library(ggrepel)
 
 # Who has how many points?
 
-ggplot(league.table, aes(Club, Pts, colour = Col))+
-  geom_point(size = 4)+
-  theme_bw()+
-  geom_text_repel(
-    label= league.table$Club, 
-    nudge_x = 0, nudge_y = -1.0, 
-    aes(colour = "black")
-  )+
-  labs(x = "Club",
-       y = "Points",
-       title = "Premier League 2020-21")+
-  theme(axis.text.x=element_blank(),
-        axis.ticks.x=element_blank())+
-  scale_color_identity()
+# ggplot(league.table, aes(Club, Pts, colour = Col))+
+#   geom_point(size = 4)+
+#   theme_bw()+
+#   geom_text_repel(
+#     label= league.table$Club, 
+#     nudge_x = 0, nudge_y = -1.0, 
+#     aes(colour = "black")
+#   )+
+#   labs(x = "Club",
+#        y = "Points",
+#        title = "Premier League 2020-21")+
+#   theme(axis.text.x=element_blank(),
+#         axis.ticks.x=element_blank())+
+#   scale_color_identity()
 
 
 ggplot(league.table, aes(Club, Pts))+
@@ -58,7 +58,7 @@ ggplot(league.table, aes(Club, GA))+
 
 ggplot(league.table, aes(GF, Pts) )+
   geom_point(size = 4, col = league.table$Col)+
-  geom_smooth(method = lm, se = F)+
+  geom_smooth(method = lm, se = FALSE, formula = y ~ x)+
   theme_bw()+
   labs(
     x = "Goals",
@@ -75,7 +75,7 @@ ggplot(league.table, aes(GF, Pts) )+
 
 ggplot(league.table, aes(GA, Pts))+
   geom_point(size = 4, col = league.table$Col)+
-  geom_smooth(method = lm, se = F)+
+  geom_smooth(method = lm, se = FALSE, formula = y ~ x)+
   theme_bw()+
   labs(
     x = "Goals",
@@ -89,15 +89,33 @@ ggplot(league.table, aes(GA, Pts))+
   scale_size_identity()
 
 
-# Home Performance vs Away Performance
+# Home Performance
 
-ggplot(league.table, aes(HW.rate, AW.Rate))+
+ggplot(league.table, aes(HW.Rate, Pts))+
   geom_point(size = 4, col = league.table$Col)+
+  geom_smooth(method = lm, se = FALSE, formula = y ~ x)+
   theme_bw()+
   labs(
-    x = "Home Win %",
-    y = "Away Win %",
-    title = "Home Vs Away")+
+    x = "Win %",
+    y = "Points",
+    title = "Home Performance")+
+  geom_text_repel(
+    label= league.table$Club, 
+    nudge_x = 0, nudge_y = -0.25, 
+    aes(size = 3)
+  )+
+  scale_size_identity()
+
+# Away Performance
+
+ggplot(league.table, aes(AW.Rate, Pts))+
+  geom_point(size = 4, col = league.table$Col)+
+  geom_smooth(method = lm, se = FALSE, formula = y ~ x)+
+  theme_bw()+
+  labs(
+    x = "Win %",
+    y = "Points",
+    title = "Away Performance")+
   geom_text_repel(
     label= league.table$Club, 
     nudge_x = 0, nudge_y = -0.25, 
@@ -109,6 +127,7 @@ ggplot(league.table, aes(HW.rate, AW.Rate))+
 
 ggplot(league.table, aes(S, ST))+
   geom_point(size = 4, col = league.table$Col)+
+  geom_smooth(method = lm, se = FALSE, formula = y ~ x)+
   theme_bw()+
   labs(
     x = "Shots",
@@ -123,12 +142,13 @@ ggplot(league.table, aes(S, ST))+
 
 # Clinical Attacks
 
-ggplot(league.table, aes(GF, ST))+
+ggplot(league.table, aes(S, (GF/S * 100)))+
   geom_point(size = 4, col = league.table$Col)+
+  geom_smooth(method = lm, se = FALSE, formula = y ~ x)+
   theme_bw()+
   labs(
-    x = "Goals Scored",
-    y = "Shots on Target",
+    x = "Shots",
+    y = "Score Rate",
     title = "Clinical Attacks")+
   geom_text_repel(
     label= league.table$Club, 
@@ -141,6 +161,7 @@ ggplot(league.table, aes(GF, ST))+
 
 ggplot(league.table, aes(F, B))+
   geom_point(size = 4, col = league.table$Col)+
+  geom_smooth(method = lm, se = FALSE, formula = y ~ x)+
   theme_bw()+
   labs(
     x = "Fouls",
@@ -152,3 +173,26 @@ ggplot(league.table, aes(F, B))+
     aes(size = 3)
   )+
   scale_size_identity()
+
+# Yellow Cards
+
+ggplot(league.table, aes(Club, YC))+
+  geom_bar(stat = "identity",
+           fill = league.table$Col)+
+  theme_bw()+
+  coord_flip()+
+  labs(x = "Club",
+       y = "Yellow Cards",
+       title = "Premier League 2020-21 Yellow Cards")
+
+# Red Cards
+
+ggplot(league.table, aes(Club, RC))+
+  geom_bar(stat = "identity",
+           fill = league.table$Col)+
+  theme_bw()+
+  coord_flip()+
+  labs(x = "Club",
+       y = "Red Cards",
+       title = "Premier League 2020-21 Red Cards")
+
